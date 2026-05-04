02f159ed19d8a8462d29f4a3034c2193c5c64421
Shivu Donmardi Gorva <48614025+Shivz3232@users.noreply.github.com>
2026-05-05 05:29:44 +0530

    Add: decode-ways - submission-3

---
+++ Data Structures & Algorithms/decode-ways/submission-3.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numDecodings(String s) {
+        if (s.charAt(0) == '0') return 0;
+        if (s.length() == 1) return 1;
+
+        // if (1 <= s.length() && s.charAt(0) <= 50 && s.charAt(1) <= 54) {
+        //     return aux(s, 1) + aux(s, 2);
+        // } else {
+        //     return aux(s, 1);
+        // }
+    }
+
+    private int aux(String s, int i) {
+        if (i >= s.length()) {
+            return 1;
+        }
+
+        if (i + 1 <= s.length() && s.charAt(i) <= 50 && s.charAt(i + 1) <= 54) {
+            return aux(s, i + 1) + aux(s, i + 2);
+        } else {
+            return aux(s, i + 1);
+        }
+    }
+}
