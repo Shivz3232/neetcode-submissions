5b7ef36555659e93e19b6ec07b69d777cfe7783a
Shivu Donmardi Gorva <48614025+Shivz3232@users.noreply.github.com>
2026-05-06 00:58:08 +0530

    Add: decode-ways - submission-7

---
+++ Data Structures & Algorithms/decode-ways/submission-7.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numDecodings(String s) {
+        if (s.charAt(0) == '0') return 0;
+        if (s.length() == 1) return 1;
+
+        return aux(s, 0);
+    }
+
+    private int aux(String s, int i) {
+        if (i >= s.length()) {
+            return 1;
+        }
+
+        if (s.charAt(i) == '0') {
+            return 0;
+        }
+
+        if (i + 1 < s.length() && s.charAt(i) <= 50 && s.charAt(i + 1) <= 54) {
+            return aux(s, i + 1) + aux(s, i + 2);
+        } else {
+            return aux(s, i + 1);
+        }
+    }
+}
