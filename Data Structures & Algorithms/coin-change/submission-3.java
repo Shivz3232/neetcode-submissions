50c0c99e898272b20052ede51bc6b2c29dd3a112
Shivu Donmardi Gorva <48614025+Shivz3232@users.noreply.github.com>
2026-05-06 02:32:47 +0530

    Add: coin-change - submission-3

---
+++ Data Structures & Algorithms/coin-change/submission-3.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0) {
+            return 0;
+        }
+
+        Map<Integer, Integer> memo = new HashMap<>();
+
+        int x = dfs(coins, amount, 0, memo);
+
+        System.out.println(amount);
+        
+        return x;
+    }
+
+    private int dfs(int[] coins, int amount, int nCoins, Map<Integer, Integer> memo) {
+        if (amount < 0) return -1;
+        if (amount == 0) return nCoins;
+        // if (memo.containsKey(amount)) return memo.get(amount);
+
+        int minCoins = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            int temp = dfs(coins, amount - coin, nCoins + 1, memo);
+            if (temp != -1 && temp < minCoins) {
+                minCoins = temp;
+            }
+        }
+
+        if (minCoins == Integer.MAX_VALUE) {
+            memo.put(amount, -1);
+            return -1;
+        } else {
+            memo.put(amount, minCoins);
+            return minCoins;
+        }
+    }
+}
