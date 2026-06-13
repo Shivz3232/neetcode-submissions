24e3bce525f1198f1f719a81a842fce4c44f4eac
Shivu Donmardi Gorva <48614025+Shivz3232@users.noreply.github.com>
2026-06-14 03:54:04 +0530

    Add: min-cost-to-connect-points - submission-2

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-2.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int minCostConnectPoints(int[][] points) {
+        int n = points.length;
+
+        int result = 0;
+
+        // Prim's Algorithm
+        Set<Integer> A = new HashSet<>();
+        PriorityQueue<Vertex> pq = new PriorityQueue<>();
+
+        // Arbitrarily chosen tree root: points[0]
+        A.add(0);
+        for (int i = 1; i < n; i++)
+            pq.add(new Vertex(i, dist(0, i, points)));
+
+        while (!pq.isEmpty()) {
+            Vertex v = pq.remove();
+
+            if (A.contains(v.id)) continue;
+
+            A.add(v.id);
+            result += v.key;
+
+            // All points with id < v.id
+            for (int i = 0; i < v.id; i++)
+                if (!A.contains(i))
+                    pq.add(new Vertex(i, dist(i, v.id, points)));
+            
+            // All points with id > v.id
+            for (int i = v.id + 1; i < n; i++)
+                if (!A.contains(i))
+                    pq.add(new Vertex(i, dist(i, v.id, points)));
+        }
+
+        return result;
+    }
+
+    private int dist(int i, int j, int[][] points) {
+        return Math.abs(
+            points[i][0] - points[j][0]
+        ) + Math.abs(
+            points[i][1] - points[j][1]
+        );
+    }
+
+    private class Vertex implements Comparable<Vertex> {
+        public final int id;
+        public final int key;
+
+        public Vertex(int id, int key) {
+            this.id = id;
+            this.key = key;
+        }
+
+        @Override
+        public int compareTo(Vertex o) {
+            return Integer.compare(this.key, o.key);
+        }
+    }
+}
