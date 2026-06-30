a135474c9b810f995f89a1293b08477aaa715ab1
Shivu Donmardi Gorva <48614025+Shivz3232@users.noreply.github.com>
2026-07-01 02:33:45 +0530

    Add: counting-bits - submission-2

---
+++ Data Structures & Algorithms/counting-bits/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] result = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            // int j = i;
+            // while (j != 0 && ++result[i] > 0)
+            //     j = (j - 1) & j;
+
+            for (int j = i; j != 0 && ++result[i] > 0; j = (j - 1) & j);
+        }
+
+        return result;
+    }
+}
