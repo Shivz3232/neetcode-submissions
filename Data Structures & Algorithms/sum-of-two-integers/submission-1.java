953ab2fb6bc0e09d764fe8ad2b9f49ba2ff162d5
Shivu Donmardi Gorva <48614025+Shivz3232@users.noreply.github.com>
2026-07-11 03:08:09 +0530

    Add: sum-of-two-integers - submission-1

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-1.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int getSum(int a, int b) {
+        boolean isNegative = false;
+        if (a < 0 && b < 0) {
+            isNegative = true;
+            a *= -1;
+            b *= -1;
+        }
+
+        int mask = 1, c = 0, result = 0;
+
+        int limit = 1 << 20;
+        while (mask <= limit) {
+            // System.out.println("Mask: " + mask + ", c: " + c + ", result: " + result);
+
+            if (c == 1) {
+                if (((mask & a) ^ (mask & b)) == 0) {
+                    result |= mask;
+                    
+                    if (((mask & a) & (mask & b)) == 0) {
+                        c = 0;
+                    }
+                }
+            } else {
+                if (((mask & a) ^ (mask & b)) != 0) {
+                    result |= mask;
+                } else {
+                    if (((mask & a) & (mask & b)) != 0) {
+                        c = 1;
+                    }
+                }
+            }
+
+            mask <<= 1;
+        }
+
+        if (isNegative) return -1 * result;
+
+        return result;
+    }
+}
