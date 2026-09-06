ef83d601aa279a5204c2380e05d79d109867fe33
Shivu Donmardi Gorva <48614025+Shivz3232@users.noreply.github.com>
2026-09-07 04:27:44 +0530

    Add: insert-new-interval - submission-0

---
+++ Data Structures & Algorithms/insert-new-interval/submission-0.java
@@ -0,0 +1,66 @@
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        int i1 = intervals.length;
+        boolean o1 = false;
+        for (int i = intervals.length - 1; i >= 0; i--) {
+            if (intervals[i][1] < newInterval[0]) {
+                i1 = i + 1;
+                break;
+            } else if (intervals[i][0] <= newInterval[0]) {
+                i1 = i;
+                o1 = true;
+                break;
+            }
+
+            i1 = i;
+        }
+
+        int i2 = -1;
+        boolean o2 = false;
+        for (int i = 0; i < intervals.length; i++) {
+            if (newInterval[1] < intervals[i][0]) {
+                i2 = i;
+                break;
+            } else if (newInterval[1] <= intervals[i][1]) {
+                i2 = i;
+                o2 = true;
+                break;
+            }
+
+            i2 = i;
+        }
+        
+        for (int i = 0; i < i1; i++) {
+            result.add(List.of(intervals[i][0], intervals[i][1]));
+        }
+
+        if (o1) {
+            result.add(new ArrayList<>());
+            result.get(result.size() - 1).add(intervals[i1][0]);
+        } else {
+            result.add(new ArrayList<>());
+            result.get(result.size() - 1).add(newInterval[0]);
+        }
+
+        if (o2) {
+            result.get(i1).add(intervals[i2][1]);
+        } else {
+            result.get(i1).add(newInterval[1]);
+        }
+
+        for (int i = o2 ? i2 + 1 : i2; i < intervals.length; i++) {
+            result.add(List.of(intervals[i][0], intervals[i][1]));
+        }
+
+        int[][] r = new int[result.size()][2];
+
+        for (int i = 0; i < result.size(); i++) {
+            r[i][0] = result.get(i).get(0);
+            r[i][1] = result.get(i).get(1);
+        }
+
+        return r;
+    }
+}
